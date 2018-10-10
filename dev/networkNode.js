f4345044a18707def7c3d1e8194bb7503a045cd6
rach <rach7089@gmail.com>
2018-10-11 03:15:41 +0900

    commit with node listening test

---
+++ dev/networkNode.js
@@ -0,0 +1,104 @@
+var rp = require('request-promise');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var Blockchain = require('./blockchain');
+var bitcoin = new Blockchain();
+var uuid = require('uuid/v1');
+var nodeAddress = uuid().split('-').join('');
+var port = process.argv[2];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+// 웹 브라우저에 get 방식으로 /blockchain 주소를 입력했을 때 실행
+
+app.get('/blockchain', function(req, res) {
+  res.send(bitcoin);
+})
+
+// 웹 브라우저에 post 방식으로 /transaction 주소를 입력했을 때 실행
+app.post('/transaction', function(req, res) {
+  const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+  res.send({note: `트랜잭션은 ${blockIndex} 블락안으로 들어갈 예정입니다.`})
+})
+
+app.get('/mine', function(req, res){
+  const lastBlock = bitcoin.getLastBlock();
+  const previousBlockHash = lastBlock['hash'];
+
+  const currentBlockData = {
+    transaction:bitcoin.pendingTransactions,
+    index:lastBlock['index']+1
+  };
+
+  const nonce = bitcoin.proofOfWork(previousBlockHash,currentBlockData);
+  const blockHash = bitcoin.hashBlock(previousBlockHash,currentBlockData,nonce);
+
+  //채굴에 대한 보상
+  bitcoin.createNewTransaction(10, "bosang0000",nodeAddress);
+  const newBlock = bitcoin.createNewBlock(nonce,previousBlockHash,blockHash);
+
+  res.json({
+    note: "새로운 블락이 성공적으로 만들어 졌습니다.",
+    newBlock: newBlock
+  })
+})
+
+// 새로운 노드를 등록하고 전체 네트워크에 알림
+
+app.post('/register-and-broadcast-node',function(req,res){
+  const newNodeUrl = req.body.newNodeUrl;
+  if (bitcoin.networkNodes.indexOf(newNodeUrl) == -1) {
+    bitcoin.networkNodes.push(newNodeUrl);
+  }
+  const reqNodesPromises = [];
+  bitcoin.networkNodes.forEach(networkNode => {
+    const requestOption = {
+      uri: networkNodesUrl + '/register-node',
+      method: 'POST',
+      body: {newNodeUrl:newNodeUrl},
+      json: true
+    };
+
+    reqNodesPromises.push(rp(requestOption))
+  });
+
+  Promise.all(reqNodesPromises)
+  .then(data=> {
+    const bulkRegiterOption = {
+      uri : newNodeUrl + '/register-nodes-bulk',
+      method : 'POST',
+      body: {allNetworkNodes : [...bitcoin.networkNodes,bitcoin.currnetNodeUrl]},
+      json: true
+    };
+
+    return rq(bulkRegiterOption);
+  }).then(data => {
+    res.json({nodt : "새로운 노드가 네트워크에 성공적으로 등록이 되었습니다."});
+  })
+})
+
+app.post('/register-node',function(req,res){
+  //새로운 노드 주소
+  const newNodeUrl = req.body.newNodeUrl;
+  //코인 네트워크에 새로운 노드의 주소가 없다면,
+  const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(newNodeUrl) == -1;
+  //코인의 현재 url이 새로운 노드 주소가 아니라면, 즉 현재 접속한 주소와 중복되지 않는다면,
+  const notCurrentNode = bitcoin.currentNodeUrl !== newNodeUrl;
+
+  if(nodeNotAlreadyPresent&&notCurrentNode){
+  //코인 전체 네트워크에 새로운 주소 등록
+  bitcoin.networkNodes.push(newNodeUrl);
+  res.json({note: "새로운 노드가 등록되었습니다."})
+  }
+
+})
+
+app.post('/register-nodes-bulk',function(req,res){
+
+})
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}...`);
+})
