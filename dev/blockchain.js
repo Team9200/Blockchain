a492ad5ee5f917961306559bbe70882075fd3d94
rach <rach7089@gmail.com>
2018-10-13 18:22:44 +0900

    update with intelligence list

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,5 +1,5 @@
 
-/*
+/*******************************************************************************
 Autor : rach
 date : 18. 10. 10
 version : v0.8
@@ -15,67 +15,70 @@ to be
 
 version info
 - 실질적으로 블록을 다른 컴퓨터에서도 조회할 수 있는 시점에 verson 1 로 업데이트 할 예정
-*/
+********************************************************************************/
 
 const sha256 = require('sha256');
 const currentNodeUrl = process.argv[3];
 
-/*
+/*******************************************************************************
   function : Blockchain 객체
   explanaion : 전체 블록체인의 정보를 구현한 객체 (ex 비트코인)
                시스템 내에서 하나만을 생성하면 될 것
-*/
+*******************************************************************************/
 
 function Blockchain() {
-  this.chain = [];                    // block을 담는 배열
-  this.pendingTransactions = [];      // 트랜잭션을 담는 배열
+  this.chain = [];                    // block을 담는 리스트
+  this.pendingTransactions = [];      // 트랜잭션을 담는 리스트
+  this.pendingIntelligence = [];      // 위협정보를 담는 리스트
   this.createNewBlock(100, '0', '0'); // genesis block 생성
   this.currentNodeUrl = currentNodeUrl;
   this.networkNodes = [];
 }
 
-/*
+/*******************************************************************************
    function : createNewBlock(nonce, previousBlockHash, hash)
    explanaion : 이전 블록의 해쉬값을 전달하고 새로운 블록을 생성하는 함수
    input : nonce - 새로운 블록의 nonce 값 (이를 계산하는 알고리즘은 추가하지 않음)
            previousBlockHash - 이전 블록의 sha-256 해쉬값
            hash - 완성된 블록의 해쉬값 (nonce 값과 비교해서 블록을 검증할 수 있음)
   return : 결과로 생성된 블록 (newBlock)
-*/
+*******************************************************************************/
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash,hash) {
   const newBlock = {
     index: this.chain.length + 1,
     timestamp: Date.now(),
     pendingTransactions: this.pendingTransactions,
+    pendingIntelligence: this.pendingIntelligence,
     nonce:nonce,
     hash:hash,
     previousBlockHash:previousBlockHash
   };
 
 this.pendingTransactions = [];                // 다음 블록을 위한 작업
+this.pendingIntelligence = [];
 this.chain.push(newBlock);
 
 return newBlock;
 }
 
-/*
+/*******************************************************************************
   function : getLastBlock()
   explanaion : 마지막 블록을 반환하는 함수
   input : NULL
   output : NULL
-*/
+*******************************************************************************/
 
 Blockchain.prototype.getLastBlock = function() {
   return this.chain[this.chain.length -1];
 }
 
-/*
+/*******************************************************************************
   function : createNewTransaction
   explanaion : 새로운 트랜잭션을 생성하는 함수
-  input : transaction 정보 (현재는 거래 정보지만 추후에 변경되어야함)
+  input : transaction 정보
   output : 생성된 트랜잭션을 담고 있는 블록 (마지막 블록)
-*/
+********************************************************************************/
 
 Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
   const newTransaction = {
@@ -88,14 +91,29 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) 
   return this.getLastBlock()['index'] + 1;
 }
 
-/*
+Blockchain.prototype.createNewIntelligence = function (analyzer, created, tags, goal, action, description) {
+  const newIntelligence = {
+    analyzer: analyzer,
+    created: created,
+    tags: tags,
+    goal: goal,
+    action: action,
+    description: description
+  }
+
+  this.pendingIntelligence.push(newIntelligence);
+  return this.getLastBlock()['index'] + 1;
+};
+
+
+/*******************************************************************************
   function : hashBlock
   explanaion : 블락의 sha-256 해쉬값을 계산하는 함수
   input : previousBlockHash - 이전 블록의 해쉬
           currentBlockData - 현재 블록의 데이터 (현재 블록을 그냥 전달)
           nonce - nonce 값 후보
   output : block의 sha-256 해쉬값
-*/
+********************************************************************************/
 
 Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
   const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
@@ -103,14 +121,14 @@ Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, n
   return hash
 }
 
-/*
+/*******************************************************************************
   function : proofOfWork
   explanaion : - nonce 값을 1씩 증가 시켜가며 난이도에 맞는 해쉬값을 계산할 때 까지 반복
               - 현재 난이도가 하드 코딩 되어 있음 추후 업데이트 필요
   input : previousBlockHash - 이전 블록의 해쉬
           currentBlockData - 현재 블록
   output : 난이도를 통과한 해쉬값
-*/
+********************************************************************************/
 
 Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
   let nonce = 0;
