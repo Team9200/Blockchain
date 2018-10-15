9a796c915e3b01ac78ae221b199bfe7b4631d2d7
rach <rach7089@gmail.com>
2018-10-15 17:11:01 +0900

    commit with modularization for mining & validation

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,18 +1,13 @@
 
 /*******************************************************************************
+filename : blockchain.js
 Autor : rach
-date : 18. 10. 14
-version : v0.86
+date : 18. 10. 15
+version : v0.87
 explanaion :
-
-as is
-- 현재 express로 웹을 이용해서 로컬상에서 블록 생성 -> 트랜잭션/인텔리전스 생성 -> 마이닝 & 결과 확인 까지 가능함
-
-to be
-- 참가 노드간 정보 공유 with p2p
-
-version info
-- 실질적으로 블록을 다른 컴퓨터에서도 조회할 수 있는 시점에 verson 1 로 업데이트 할 예정
+  - 블록체인 구조체를 정의한 파일
+  - 트랜잭션, 악성코드 정보를 추가하는 메소드들이 정의되어 있음
+  - 마이닝, 검증 메소드는 mine.js로 분리함
 
 ********************************************************************************/
 
@@ -117,40 +112,5 @@ Blockchain.prototype.addNewMalware = function (malware) {
   this.malwaresList.push(newMalware);
   return this.getLastBlock()['index'] + 1;
 };
-
-
-/*******************************************************************************
-  function : hashBlock
-  explanaion : 블락의 sha-256 해쉬값을 계산하는 함수
-  input : previousBlockHash - 이전 블록의 해쉬
-          currentBlockData - 현재 블록의 데이터 (현재 블록을 그냥 전달)
-          nonce - nonce 값 후보
-  output : block의 sha-256 해쉬값
-********************************************************************************/
-
-Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
-  const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
-  const hash = sha256(dataAsString);
-  return hash
-}
-
-/*******************************************************************************
-  function : proofOfWork
-  explanaion : - nonce 값을 1씩 증가 시켜가며 난이도에 맞는 해쉬값을 계산할 때 까지 반복
-              - 현재 난이도가 하드 코딩 되어 있음 추후 업데이트 필요
-  input : previousBlockHash - 이전 블록의 해쉬
-          currentBlockData - 현재 블록
-  output : 난이도를 통과한 해쉬값
-********************************************************************************/
-
-Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
-  let nonce = 0;
-  let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
-  while(hash.substring(0,4) != '0000') {
-    nonce++;
-    hash = this.hashBlock(previousBlockHash,currentBlockData,nonce);
-  }
-  return nonce;
-}
 
 module.exports = Blockchain;
