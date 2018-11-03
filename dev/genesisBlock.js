cbbc908a018039a76e2661fc10b2d2260641e29b
rach <rach7089@gmail.com>
2018-11-03 19:40:23 +0900

    update wirh detail comment

--- dev/genesisBlock.js
+++ dev/genesisBlock.js
@@ -1,3 +1,13 @@
+/*******************************************************************************
+filename : genesisBlock.js
+Autor : rach
+date : 18. 11. 03
+version : v1.0
+explanaion :
+- 제네시스 블락을 생성하는 코드
+
+********************************************************************************/
+
 var fs = require('fs');
 var Blockchain = require('./blockchain');
 
@@ -6,5 +16,7 @@ var make = function() {
   var firstChain = JSON.stringify(genesis);
   fs.writeFileSync("genesisChain.json", firstChain);
 }
+
+make();
 
 exports.make = make;
--- dev/readWriteChain.js
+++ dev/readWriteChain.js
@@ -1,3 +1,14 @@
+
+/*******************************************************************************
+filename : readWriteChain.js
+Autor : rach
+date : 18. 11. 03
+version : v1.0
+explanaion :
+  - json 포맷의 체인을 읽을 수 있도록 구현
+  - 만들어진 체인을 json 파일로 쓸 수 있게 구현
+********************************************************************************/
+
 var fs = require('fs');
 var Blockchain = require('./mine');
 var marlist = [];
@@ -8,12 +19,27 @@ Blockchain.prototype.Blockchain = function (input) {
   this.pendingMalwares = input.pendingMalwares;
 };
 
+/*******************************************************************************
+   function : readChain(filename)
+   explanaion : json 형태로 저장된 체인을 읽을 수 있도록 하는 함수
+   input : filename - 읽을 파일의 이름
+   return : Blockchain 객체
+*******************************************************************************/
+
 var readChain = function(filename) {
   var localChain = fs.readFileSync(filename);
   localChain = JSON.parse(localChain);
   localChain = new Blockchain(localChain);
   return localChain;
 }
+
+/*******************************************************************************
+   function : writeChain(localChain, filename)
+   explanaion : 블록체인 객체를 json 형태로 저장하는 함수
+   input : localChain - json 으로 변환할 블록체인의 인스턴스
+           filename - 저장할 파일의 이름
+   return : void
+*******************************************************************************/
 
 var writeChain = function(localChain, filename){
     var localChainFile = JSON.stringify(localChain);
