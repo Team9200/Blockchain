2a9df37cc0742504abc7f505f6ea852e3ce8fd34
rach <rach7089@gmail.com>
2018-11-11 00:46:40 +0900

    verification modulazied

--- dev/mine.js
+++ dev/mine.js
@@ -20,40 +20,6 @@ Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, n
 }
 
 /*******************************************************************************
-  function: verifyTransaction
-  explanaion: 트랜잭션의 유효성을 판단하는 함수
-  input : 유효성을 확인하고자 하는 트랜잭션
-  output: true, false
-*******************************************************************************/
-
-function verifyTransaction(transaction) {
-  const utxo = searchInput();
-  if (function searchInput(){} == false) return false;
-  if
-}
-
-/*******************************************************************************
-  function: isBlockValid
-  explanaion: 블락의 sha -256 해쉬값이 올바른지 확인하는 함수
-  input : 확인하고자 하는 블락
-  output: true, false
-
-  note : 이 함수는 다른 모듈로 있는게 좋을 것 같다.
-         hashBlock 과 함께
-*******************************************************************************/
-
-function isBlockValid(block) {};
-
-/*******************************************************************************
-  function: isChainValid
-  explanaion: blockchain의 block 들이 유효하게 연결되어 있는지 확인하는 함수
-  input:
-  output: true, false
-*******************************************************************************/
-
-Blockchain.prototype.isChainValid = function () {};
-
-/*******************************************************************************
   function : proofOfWork
   explanaion : - nonce 값을 1씩 증가 시켜가며 난이도에 맞는 해쉬값을 계산할 때 까지 반복
               - 현재 난이도가 하드 코딩 되어 있음 추후 업데이트 필요
