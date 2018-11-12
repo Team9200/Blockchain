8035afff7b5260b685df3e7237fc889aa8638dbd
rach <rach7089@gmail.com>
2018-11-12 16:29:30 +0900

    commit with comment, correct error

--- dev/sign.js
+++ dev/sign.js
@@ -1,13 +1,18 @@
 const secp256k1 = require('secp256k1')
 const bs58check = require('bs58check');
 
-function signTransaction(transaction, address) {
-  let pubKey = bs58check.decode(address);
+/*******************************************************************************
+  function : signTransaction
+  explanaion : 트랜잭션 아이디(트랜잭션의 해쉬의 앞에서 부터 절반 자른값)을 개인키로
+              서명하는 함수
+  input : transaction(json), privkey(buffer array)
+  output : signiture(buffer array)
+********************************************************************************/
+
+function signTransaction(transaction, privatekey) {
   let msg = Buffer.from(transaction[tx_id].slice(0,32));
-  let sign = secp256k1.sign(msg,privKeyBuffer);
+  let sign = secp256k1.sign(msg, privatekey);
   return sign.signature;
 }
 
-function verifySign(msg, signature, pubKey) {
-  return secp256k1.verify(msg, signature, pubKey);
-}
+module.exports = signTransaction;
